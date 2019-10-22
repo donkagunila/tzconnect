f21ae4a1dc4ebc0d8949b8ff7e22f4238cf10ff8
donkagunila <donkagunila@gmail.com>
2019-10-22 06:30:51 +0300

    right alig

---
+++ resources/views/site/contact.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.site')
+
+@section('title', 'About Us | Tzconect')
+
+@section('description', 'Lets Connect, we bring together services, Events and Providers closer than ever')
+
+@section('content')
+
+  <div id="contactUsMap" class="big-map"></div>
+	
+
+	<div class="main">
+		<div class="container">
+			<section>
+				<div class="about-services features-2">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto text-center">
+            <h2 class="title">We Connect the best</h2>
+            <h5 class="description">We are a group of enthusiasts who came together to close the berrier between a customer and a service, we bring together all kinds of services related to events and ceremonies</h5>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="info info-horizontal">
+              <div class="icon icon-rose">
+                <i class="material-icons">gesture</i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">1. Connect</h4>
+                <p>we bring together all kinds of services related to events and ceremonies and get you the easiest way you can plan</p>
+                
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="info info-horizontal">
+              <div class="icon icon-rose">
+                <i class="material-icons">build</i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">2. Develop</h4>
+                <p>We develop a sense of belonging we bring together all kinds of services related to events and ceremonies</p>
+               
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="info info-horizontal">
+              <div class="icon icon-rose">
+                <i class="material-icons">mode_edit</i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">3. Inspire</h4>
+                <p>we bring together all kinds of services related to events and ceremonies, with a tone of inspirations we give you the best</p>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+			</section>
+		</div>
+	</div>
+
+	
+@endsection
---
+++ resources/views/site/inc/header.blade.php
@@ -0,0 +1,65 @@
+  <nav class="navbar navbar-color-on-scroll navbar-transparent    fixed-top  navbar-expand-lg " color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="../index-2.html">
+          TzConnect </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="dropdown nav-item">
+            <a href="#" class="nav-link">
+              <i class="material-icons">apps</i> Services
+            </a>
+          </li>
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">view_day</i> Vendors
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">dns</i> Catering
+              </a>
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">build</i> Halls and Decoration
+              </a>
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">list</i> Entertainment
+              </a>
+             
+            </div>
+          </li>
+          <li class="dropdown nav-item">
+            <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+              <i class="material-icons">view_carousel</i> Events
+            </a>
+            <div class="dropdown-menu dropdown-with-icons">
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">account_balance</i> Corporate events
+              </a>
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">art_track</i> Individuals
+              </a>
+              <a href="#" class="dropdown-item">
+                <i class="material-icons">view_quilt</i> Archive
+              </a>
+            </div>
+          </li>
+          
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="button-container nav-item iframe-extern">
+            <a href="#" target="_blank" class="btn  btn-rose   btn-round btn-block">
+              <i class="icofont-login"></i> Get started
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
